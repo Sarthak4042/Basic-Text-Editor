c88eb22670f63b8d6d6dfe20e24f9fc4f7bdfe83
Sarthak4042 <95751032+Sarthak4042@users.noreply.github.com>
2023-09-18 02:47:30 +0530

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def new_file():
+    text.delete(1.0, tk.END)
+
+def open_file():
+    file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    if file_path:
+        with open(file_path, 'r') as file:
+            text.delete(1.0, tk.END)
+            text.insert(tk.END, file.read())
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    if file_path:
+        with open(file_path, 'w') as file:
+            file.write(text.get(1.0, tk.END))
+
+def cut_text():
+    text.event_generate("<<Cut>>")
+
+def copy_text():
+    text.event_generate("<<Copy>>")
+
+def paste_text():
+    text.event_generate("<<Paste>>")
+
+root = tk.Tk()
+root.title("Basic Text Editor")
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+edit_menu = tk.Menu(menu)
+menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut", command=cut_text)
+edit_menu.add_command(label="Copy", command=copy_text)
+edit_menu.add_command(label="Paste", command=paste_text)
+
+text = tk.Text(root)
+text.pack(expand="true", fill="both")
+
+root.mainloop()
